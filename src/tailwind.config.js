59829a97d1af69394a056d84f805fb809fa77e75
Ion Claudiu Alexandru <onixenic@gmail.com>
2025-10-16 23:52:00 +0200

    0.0.0 - init

---
+++ src/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Geist', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
